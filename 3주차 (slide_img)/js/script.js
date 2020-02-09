e9d04de8213da9b58fd209681a8e612c28b5d8a6
Jangyusu <dbtnss@naver.com>
2020-02-09 20:39:02 +0900

    주석 수정

--- 3주차 (slide_img)/js/script.js
+++ 3주차 (slide_img)/js/script.js
@@ -10,15 +10,9 @@ var slideCount = slides.length;
 
 var slideWrapper = document.getElementsByClassName("container");
 
-////////////////////// var 선언 //////////////////////////////
-
-height(); // li의 높이를 자동으로 조정해준다.
-
 function slide(idx) {
     currentIndex = idx;
     slideContainer[0].style.left = idx * -100 + "%";
-
-    // buttonHide();
 }
 
 for (var i = 0; i < slideCount; i++) {
@@ -57,20 +51,6 @@ function buttonHide() {
     }
 }
 
-// 슬라이드 이동 함수
-
-// 버튼을 클릭하면 슬라이드 이동시키기
-
-// 버튼 사라지게 하기
-
-// (추가) 첫번째 슬라이드 먼저 보이도록 하기
-
-// (추가) 슬라이드가 있으면 가로로 배열하기
-
-// (추가) 슬라이드의 높이 확인하여 부모의 높이로 지정하기
-
-// (추가) 처음에서 뒤로가는 버튼 누르면 마지막으로 가기 (마지막도 동일)
-
 function height() {
     for (var j = 0; j < slideCount; j++) {
         if (topHeight < slides[j].offsetHeight) {
@@ -80,3 +60,5 @@ function height() {
     slideWrapper[0].style.height = topHeight + "px";
     slideContainer[0].style.height = topHeight + "px";
 }
+
+height(); // li의 높이를 자동으로 조정해준다.
