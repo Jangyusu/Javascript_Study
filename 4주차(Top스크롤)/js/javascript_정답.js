63ecb36f1ec34b51e66cd31e4d9a6e0a49aa9f0a
Jangyusu <dbtnss@naver.com>
2020-02-16 17:45:49 +0900

    4주차 스터디 완료

--- 4주차(Top스크롤)/js/javascript_정답.js
+++ 4주차(Top스크롤)/js/javascript_정답.js
@@ -4,6 +4,8 @@ var btt = document.getElementById("back-to-top"),
     scrollPos,
     docHeight;
 
+console.log(docElem);
+
 docHeight = Math.max(docElem.offsetHeight, docElem.scrollHeight);
 
 if (docHeight !== "") {
@@ -15,10 +17,13 @@ window.addEventListener("scroll", function() {
     btt.className = scrollPos > offset ? "visible" : "";
 });
 
+//
+
 btt.addEventListener("click", function(e) {
     e.preventDefault();
     scrollToTop();
 });
+
 function scrollToTop() {
     var scrollInterval = setInterval(function() {
         if (scrollPos != 0) {
---
+++ 4주차(타이핑효과)/js/textRemove.js
@@ -0,0 +1,38 @@
+let i = 0;
+let j = 0;
+const textArray = ["HTML", "CSS", "Javascript"],
+    speed = 100,
+    target = document.getElementById("type");
+
+//한글자씩 추출해서 html에 출력 charAt
+function type() {
+    if (i < textArray[j].length) {
+        target.innerHTML += textArray[j].charAt(i);
+        i++;
+        setTimeout(type, 100);
+    } else {
+        remove();
+    }
+}
+
+//한글자씩 html에서 제거 slice
+function remove() {
+    if (0 <= i) {
+        target.innerHTML = textArray[j].slice(0, i);
+        i--;
+        setTimeout(remove, 100);
+    } else {
+        textNum();
+        type();
+    }
+}
+//다음 문자열로 넘어감
+function textNum() {
+    if (j === textArray.length - 1) {
+        j = 0;
+    } else {
+        j++;
+    }
+}
+
+type();
